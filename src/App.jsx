772f422f8a23b6a9946492bbcaa88ea6f22c6b5f
jasonmueller <jmuel9999@gmail.com>
2021-07-27 17:43:20 -0500

    Added states to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
+import React, {useState} from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
 
 
 function App() {
+  const [about, setAbout] = useState(true);
+  const [portfolio, setPortfolio] = useState(false);
+  const [contact, setContact] = useState(false);
+
   return (
     <main className="app">
-      <Navbar></Navbar>
-      <About></About>
-      <Contact></Contact>
+      <Navbar about={about} setAbout={setAbout} portfolio={portfolio} setPortfolio={setPortfolio} contact={contact} setContact={setContact}></Navbar>
+      <div>
+        {about ? (
+          <>
+            <About></About>
+          </>
+        ) 
+        : portfolio ? (
+          <>
+            <Portfolio></Portfolio>
+          </>
+        ) 
+        : (
+          <>
+            <Contact></Contact>
+          </>
+        )}
+      </div>
       <Footer></Footer>
     </main>
   );
