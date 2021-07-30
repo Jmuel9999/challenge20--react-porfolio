016d6ed89bb1d9fc0691982a4497c6e93b50fc1e
jasonmueller <jmuel9999@gmail.com>
2021-07-29 21:27:31 -0500

    Added more info to pages

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,6 +6,14 @@ export default function Contact() {
     return (
         <div className="contact">
             <h1>Contact Me</h1>
+            <br></br>
+            <div className="contactInfo">
+                <p>Phone Number: 920-210-7365</p>
+                <br></br>
+                <p>Email: Jmuel9999@gmail.com</p>
+                <br></br>
+                <p>GitHub: https://github.com/Jmuel9999 </p>
+            </div>
         </div>
     )
 }
