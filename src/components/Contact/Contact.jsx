9abe8188e8488162b7f4b312ef14c1e3383fa5f5
jasonmueller <jmuel9999@gmail.com>
2021-08-01 08:38:52 -0500

    Contact section updated

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,28 @@
-import "./contact.scss"
+import "./contact.scss";
+import React, { useState } from "react";
 
 export default function Contact() {
     return (
-        <div class="container">
-            
-
-           
-           
+        <div className="contactDiv">
+            <div className="contactContainer">
+                
+                <form className="contactForm">
+                    <p>Reach out to me!</p>
+                    <div className="inputField">
+                        <label className="inputName" for="name">Name:</label>
+                        <input type="text" id="name" placeholder="Enter your name here."></input>
+                    </div>
+                    <div className="inputField">
+                        <label className="inputName" for="name">Email:</label>
+                        <input type="text" id="name" placeholder="Enter your email here."></input>
+                    </div>
+                    <div className="inputField">
+                        <label className="inputName" for="name">Message:</label>
+                        <textarea type="text" id="name" placeholder="Questions?"></textarea>
+                    </div>
+                    <button className="contactSubmitBtn" type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
