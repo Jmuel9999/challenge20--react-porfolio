b5287fb2bea960a7984cf83846e460847ec538db
jasonmueller <jmuel9999@gmail.com>
2021-08-01 09:15:46 -0500

    Footer links created, and tested OK

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,10 @@ import "./footer.scss"
 
 export default function footer() {
     return (
-        <div className="footer">
-            
-        </div>
+        <div className="footerDiv">
+            <a className="link" href="https://github.com/Jmuel9999">GitHub</a>
+            <a className="link" href="https://www.linkedin.com/feed/">LinkIn</a>
+            <a className="link" href="https://stackoverflow.com/users/14968147/jmuel9999">Stack Overflow</a>
+        </div>    
     )
 }
