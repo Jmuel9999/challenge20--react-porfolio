c3fcf1177b00760212af9edadddc10c1a000dbdd
jasonmueller <jmuel9999@gmail.com>
2021-08-01 12:11:44 -0500

    Spread out the different projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,10 +5,10 @@ function Projects ( props ) {
         <div className="projectDiv">
             <h3 className="deployed"> { props.title }</h3>
             <a href={ props.link }>
-                <h4 className="deployed">Website</h4>
+                <h4 className="webLink">Website</h4>
             </a>
             <a href={ props.github }>
-                <h4 className="deployed">Github</h4>
+                <h4 className="githubLink">Github</h4>
             </a>
         </div>
     )
