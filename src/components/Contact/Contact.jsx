7aad06f4e0e7bcec72b1031b4f1976af14a6de88
jasonmueller <jmuel9999@gmail.com>
2021-08-01 08:10:33 -0500

    Added helpers.js file in utils directory

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,12 @@
 import "./contact.scss"
 
 export default function Contact() {
-
-
     return (
-        <div className="contact">
-            <h1>Contact Me</h1>
-            <br></br>
-            <div className="contactInfo">
-                <p>Phone Number: 920-210-7365</p>
-                <br></br>
-                <p>Email: Jmuel9999@gmail.com</p>
-                <br></br>
-                <p>GitHub: https://github.com/Jmuel9999 </p>
-            </div>
+        <div class="container">
+            
+
+           
+           
         </div>
     )
 }
